9fc60a08e642e30e1dcaf145a1774792cc2f5c56
MarciolimaDev <marclima998@gmail.com>
2024-03-07 16:05:04 -0500

    criação do projeto e app

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY
+#SECRET_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
