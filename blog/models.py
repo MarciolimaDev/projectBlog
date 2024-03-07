7413015b21f7abb3a0ab892e9e2aac5e73f50c84
MarciolimaDev <marclima998@gmail.com>
2024-03-07 17:50:01 -0500

    primeiro model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import dates
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    subTitle = models.CharField(max_length=255)
+    description = models.TextField()
+    dateAt = models.DateTimeField(default=datetime.now())
